736fc0a6a11eb3c116f22ba829fa26b7e7d6e27f
Jonathan Frederick <jrfreder@gmail.com>
2016-02-03 14:33:40 -0500

    wrote test for built-in user model

--- listapp/tests.py
+++ listapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    # Entire user story will go here
+    def setUp(self):
+        test_user = User.objects.create(
+            username="brother.bear",
+            password="password",
+            first_name="Brother Bear",
+            last_name="Berenstain",
+        )
+
+    # Checks that users are being created in the database
+    def test_user_creation(self):
+        self.assertEquals(User.objects.get(username='brother.bear').first_name, 'Brother Bear')
+        self.assertEquals(User.objects.get(username='brother.bear').last_name, 'Berenstain')
