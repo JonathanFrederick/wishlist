692c753f585b8f90ca94b3058c37591df1f68e91
Jonathan Frederick <jrfreder@gmail.com>
2016-02-03 13:33:58 -0500

    successfully getting blank table in Postico

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wishlist',
+        'USER': 'wishlist',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
